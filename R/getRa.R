46981f18c8ebedea96a436be8d78b5ec4a67db23
AR <redondohernandezalberto@gmail.com>
2017-06-24 17:56:16 +0200

    Removed errors and improved performance of getRa

--- R/getRa.R
+++ R/getRa.R
@@ -8,19 +8,9 @@
 #' @examples
 #' getRa(x)
 getRa <- function(emailMatrix,mail = c(1,0,1,0,1)){
-  mail = as.factor(mail)
   # Check all the emails to find in matrix to find the imput email
   for (i in 1:nrow(emailMatrix)){
-    counter=0
-    # Check if all the values are equal
-    for (p in 1:length(mail)){
-      # Increase the counter if values are equal
-      if (emailMatrix[i,p]==mail[p]){
-        counter=counter+1
-      }
-    }
-    # If all the values match, the counter should be equal to email length
-    if (counter==length(mail)){
+    if (all(emailMatrix[i,1:length(mail)]==mail)){
       ra = emailMatrix[i,length(mail)+1]
     }
   }
